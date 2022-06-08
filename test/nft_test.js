123c74997b1f7d43369e5687c691dbdde2c577af
Lorenzobattistela <lorenzowottrich@gmail.com>
2022-06-07 21:21:03 -0300

    delete

--- test/nft_test.js
+++ test/nft_test.js
@@ -15,7 +15,7 @@ describe("Nft", function() {
             const Nft = await hre.ethers.getContractFactory("NFT");
             const nft = await Nft.deploy("Melk test", "MELK", "baseURIhere");
             await nft.deployed();
-            tx = await nft.mint("0x422F4B687050f60DfAA64BF46AabEf9dEE9605aB", 1);
+            tx = await nft.mint(_mintAmount=1);
             console.log(tx)
         })
     })
